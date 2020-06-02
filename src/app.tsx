142da6772bea667b48aedffdddcdb0e3c809d9e5
So魚丸丷 <dygood@outlook.com>
2020-06-02 11:12:28 +0800

    删掉了一些东西.

--- src/app.tsx
+++ src/app.tsx
@@ -2,12 +2,8 @@
 
 import { AvatarGif, LogoPng } from '@/assets';
 import { BaseUrl, ExpiredTime } from '@/configs';
-import {
-  BasicLayoutProps,
-  Settings as LayoutSettings
-} from '@ant-design/pro-layout';
+import { BasicLayoutProps, Settings as LayoutSettings } from '@ant-design/pro-layout';
 import { RequestConfig, history } from 'umi';
-import { message, notification } from 'antd';
 
 import Cookies from 'js-cookie';
 import Footer from '@/components/Footer';
@@ -15,6 +11,7 @@ import { LoadUser } from '@/services/user';
 import React from 'react';
 import RightContent from '@/components/RightContent';
 import defaultSettings from '../config/default';
+import { message } from 'antd';
 
 //#region InitialState
 /**
@@ -75,43 +72,42 @@ export const layout = ({ initialState }: { initialState: { settings?: LayoutSett
 /**
  * CodeMessage
  */
-const codeMessage = {
-  200: '服务器成功返回请求的数据。',
-  201: '新建或修改数据成功。',
-  202: '一个请求已经进入后台排队（异步任务）。',
-  204: '删除数据成功。',
-  400: '发出的请求有错误，服务器没有进行新建或修改数据的操作。',
-  401: '用户没有权限（令牌、用户名、密码错误）。',
-  403: '用户得到授权，但是访问是被禁止的。',
-  404: '发出的请求针对的是不存在的记录，服务器没有进行操作。',
-  405: '请求方法不被允许。',
-  406: '请求的格式不可得。',
-  410: '请求的资源被永久删除，且不会再得到的。',
-  422: '当创建一个对象时，发生一个验证错误。',
-  500: '服务器发生错误，请检查服务器。',
-  502: '网关错误。',
-  503: '服务不可用，服务器暂时过载或维护。',
-  504: '网关超时。'
-};
+// const codeMessage = {
+//   200: '服务器成功返回请求的数据。',
+//   201: '新建或修改数据成功。',
+//   202: '一个请求已经进入后台排队（异步任务）。',
+//   204: '删除数据成功。',
+//   400: '发出的请求有错误，服务器没有进行新建或修改数据的操作。',
+//   401: '用户没有权限（令牌、用户名、密码错误）。',
+//   403: '用户得到授权，但是访问是被禁止的。',
+//   404: '发出的请求针对的是不存在的记录，服务器没有进行操作。',
+//   405: '请求方法不被允许。',
+//   406: '请求的格式不可得。',
+//   410: '请求的资源被永久删除，且不会再得到的。',
+//   422: '当创建一个对象时，发生一个验证错误。',
+//   500: '服务器发生错误，请检查服务器。',
+//   502: '网关错误。',
+//   503: '服务不可用，服务器暂时过载或维护。',
+//   504: '网关超时。'
+// };
 
 /**
  * 运行时request配置
  */
 export const request: RequestConfig = {
-  timeout: 10000,
-  // 当接口规范时,采用配置errorHandler的方式来实现错误提示.
-  errorHandler: (error: { response: Response }) => {
-    const { response } = error;
-    if (response && response.status) {
-      const errorText = codeMessage[response.status] || response.statusText;
-      const { status, url } = response;
-      notification.error({ message: `请求错误 ${status}: ${url}`, description: errorText });
-    }
-    if (!response) {
-      notification.error({ message: '网络异常', description: '您的网络发生异常，无法连接服务器' });
-    }
-    throw error;
-  },
+  // timeout: 10000,
+  // errorHandler: (error: { response: Response }) => {
+  //   const { response } = error;
+  //   if (response && response.status) {
+  //     const errorText = codeMessage[response.status] || response.statusText;
+  //     const { status, url } = response;
+  //     notification.error({ message: `请求错误 ${status}: ${url}`, description: errorText });
+  //   }
+  //   if (!response) {
+  //     notification.error({ message: '网络异常', description: '您的网络发生异常，无法连接服务器' });
+  //   }
+  //   throw error;
+  // },
   // 当接口不规范时,采用配置errorConfig的方式来实现错误提示.
   errorConfig: {
     adaptor: (res: any) => {
@@ -124,6 +120,7 @@ export const request: RequestConfig = {
     errorPage: '1'
   },
   prefix: BaseUrl,
+  credentials: 'include',
   // 中间件
   middlewares: [],
   // 请求拦截器
