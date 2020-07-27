abfa7d1cef46ac9fe65bb5a8e171c79bf4cddeeb
1585955375@qq.com <1585955375@qq.com>
2020-07-27 21:26:34 +0800

    【Add】添加公共按钮组件，添加在子组件调用父组件的方法，并传值

---
+++ src/components/ButtonBar/index.tsx
@@ -0,0 +1,38 @@
+import React,{Component} from 'react';
+
+import { Button } from 'antd';
+
+const button:Array<{key:string,clickName:string,style:string}>=[
+  {key:"add",clickName:"add",style:"1231"},
+  {key:"update",clickName:"update",style:"1231"},
+]
+class ButtonBar extends Component{
+    constructor(props: Readonly<{}>){
+        super(props);
+    }
+    itemclick:any;//每次设定点击的那个方法
+    btnclick=  (record:any) =>{
+      this.itemclick=record;
+      //调用父组件的方法，父组件内需要定义一个getFun
+      this.props.getFun();
+    }
+    render(){
+        return(
+          <div>
+          {button.map(item=>{
+            return (
+              <Button 
+              key={item.key}
+              onClick={()=>{
+                this.btnclick(item.clickName);//动态渲染按钮
+              }}
+              >
+              {item.key}
+              </Button>
+            )
+          })} 
+          </div>
+        )
+    }
+}
+export default ButtonBar;
