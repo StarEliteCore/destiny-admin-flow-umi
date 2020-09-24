b71108a5c11231b21047e79ec210320e5c200cf9
1585955375@qq.com <1585955375@qq.com>
2020-09-24 22:21:29 +0800

    init

--- src/configs/index.ts
+++ src/configs/index.ts
@@ -3,5 +3,5 @@
 // export const BaseUrl: string = 'http://192.168.0.2:50003/api/';
 export const BaseUrl: string = process.env.NODE_ENV == 'development' ? 'http://localhost:50003/api/' : 'http://api.destinycore.club/api/';
 // export const BaseUrl: string = 'http://localhost:50003/api/';
-
+console.log(process.env.NODE_ENV);
 export const IconFontUrl: Array<string> | string = ['//at.alicdn.com/t/font_1852099_wnyhycs81a.js'];
